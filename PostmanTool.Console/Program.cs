6ebcf5a5fda19f0faf772969665baabf779ffaa1
yvan.zhang <yvan.zhang@dji.com>
2021-06-29 21:09:31 +0800

    first commit

---
+++ PostmanTool.Console/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PostmanTool.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("加载文件，输入文件路径：");
+            var filePath = System.Console.ReadLine();
+
+            var contentArr = File.ReadAllLines(filePath);
+
+            List<Model.ItemItem> items = new List<Model.ItemItem>();
+
+            foreach (var content in contentArr)
+            {
+                System.Console.Write(content);
+
+                var contentSplArr = content.Split('^');
+                Guid mstId = new Guid(contentSplArr[0]);
+                string orderCode = contentSplArr[1];
+
+                Model.ItemItem item = new Model.ItemItem
+                {
+                    name = "LMSRule/Storage/SyncMES - " + orderCode,
+                    request = new Model.Request
+                    {
+                        method = "POST",
+                        header = new List<string>(),
+                        url = new Model.Url
+                        {
+                            raw = "https://lms.djicorp.com/LMSRule/Storage/SyncMES",
+                            host = new List<string> { "lms", "djicorp", "com" },
+                            path = new List<string> { "LMSRule", "Storage", "SyncMES" },
+                            protocol = "https"
+                        },
+                        body = new Model.Body
+                        {
+                            mode = "raw",
+                            options = new Model.Options
+                            {
+                                raw = new Model.Raw
+                                {
+                                    language = "json"
+                                }
+                            },
+                            raw = Newtonsoft.Json.JsonConvert.SerializeObject(new { MstGuid = mstId })
+                        }
+                    }
+                };
+                items.Add(item);
+            }
+
+            Model.Root root = new Model.Root
+            {
+                info = new Model.Info
+                {
+                    name = "MIA推送MES",
+                    schema = "https://schema.getpostman.com/json/collection/v2.1.0/collection.json",
+                    _postman_id = "ebcd6dcd-4258-4f71-9ba6-6fa0b448fd63"
+                },
+                item = items
+            };
+
+            File.WriteAllText("E:\\Users\\yvan.zhang\\Desktop\\20210629 历史MIA单推送MES\\res.txt", Newtonsoft.Json.JsonConvert.SerializeObject(root));
+
+            System.Console.Write("处理成功");
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ PostmanTool.Model/Model.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+
+namespace PostmanTool.Model
+{
+    public class Info
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string _postman_id { get; set; }
+        /// <summary>
+        /// MIA推送MES
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string schema { get; set; }
+    }
+
+    public class Raw
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string language { get; set; }
+    }
+
+    public class Options
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Raw raw { get; set; }
+    }
+
+    public class Body
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string mode { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string raw { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Options options { get; set; }
+    }
+
+    public class Url
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string raw { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string protocol { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<string> host { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<string> path { get; set; }
+    }
+
+    public class Request
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string method { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<string> header { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Body body { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Url url { get; set; }
+    }
+
+    public class ItemItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Request request { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<string> response { get; set; }
+    }
+
+    public class Root
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Info info { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<ItemItem> item { get; set; }
+    }
+}
